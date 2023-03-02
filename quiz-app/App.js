8b1e04133590172a4a4be98ba4e6e6410895e86a
Mahendra <103992158+maddyyyyyyy6@users.noreply.github.com>
2023-03-02 14:00:09 +0000

    refactored the app.js

--- quiz-app/App.js
+++ quiz-app/App.js
@@ -1,28 +1,25 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { StyleSheet, Text, View } from 'react-native';
-import MyStack from './navigation';
-import Home from "./screens/Home"
-import Quiz from './screens/Quiz';
-import Result from './screens/Result';
+import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet, Text, View } from "react-native";
+import MyStack from "./navigation";
 
 export default function App() {
-  return (
-      <NavigationContainer>
-        <MyStack />
-      </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <MyStack />
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    // alignItems: 'center',
-    paddingTop: 31,
-    paddingHorizontal: 16
-    // justifyContent: 'center',
-  },
-  text: {
-    fontSize: 26,
-  }
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        // alignItems: 'center',
+        paddingTop: 31,
+        paddingHorizontal: 16,
+        // justifyContent: 'center',
+    },
+    text: {
+        fontSize: 16,
+    },
 });
